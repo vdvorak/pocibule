325b83209eedb77afabdac3824bbe259057d0bec
Vitek Dvorak <vitek.dvorak@navigo3.com>
2024-06-09 08:23:39 +0200

    editable nodes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css"
 
 const ROOT = document.querySelector("#app")
+let dialogElement = null
 
 const COLORS = {
   YELLOW: "#ffeb3b",
@@ -12,8 +13,9 @@ const COLORS = {
 let LAST_BOARD_ID = 0
 let LAST_COLUMN_ID = 0
 let LAST_NODE_ID = 0
+let TEXTURE = "./bg.webp"
+
 let boards = {}
-let TEXTURE = "./vite.svg"
 
 load()
 
@@ -43,6 +45,11 @@ function load() {
   const buttonAddColumn = document.createElement("button")
   const buttonAddNode = document.createElement("button")
 
+  dialogElement = document.createElement("dialog")
+  dialogElement.classList.add("modal")
+
+  ROOT.append(dialogElement)
+
   buttonAddBoard.innerText = "Add board"
   buttonAddColumn.innerText = "Add column"
   buttonAddNode.innerText = "Add node"
@@ -70,10 +77,9 @@ function addBoard(title, colWidth = 300) {
   return id
 }
 
-function addColumn(boardIdInput, title) {
+function addColumn(boardIdInput, title = "New column") {
   const boardId = boardIdInput || `b${LAST_BOARD_ID - 1}`
   const id = `${boardId}-c${LAST_COLUMN_ID++}`
-  console.log(boards, boardId)
   boards[boardId].cols[id] = {
     boardId,
     id,
@@ -94,6 +100,8 @@ function addNode(boardIdInput, title = "New node", color = COLORS.YELLOW) {
     title,
     posX: 20,
     posY: 20,
+    width: 100,
+    height: 50,
     color,
   }
   save()
@@ -103,7 +111,6 @@ function addNode(boardIdInput, title = "New node", color = COLORS.YELLOW) {
 
 function createBoard(board) {
   const boardElement = document.createElement("div")
-  const imgElement = document.createElement("img")
 
   boardElement.id = board.id
   boardElement.classList.add("board")
@@ -144,6 +151,16 @@ function createNode(node) {
   nodeElement.style.background = node.color
   nodeElement.style.left = node.posX + "px"
   nodeElement.style.top = node.posY + "px"
+  nodeElement.style.width = node.width + "px"
+  nodeElement.style.height = node.height + "px"
+
+  nodeElement.ondblclick = () => editNode(node.boardId, node.id)
+
+  new ResizeObserver((e) => {
+    node.width = e[0]?.contentRect?.width || node.width
+    node.height = e[0]?.contentRect?.height || node.height
+    save()
+  }).observe(nodeElement)
 
   nodeElement.addEventListener("dragstart", (e) => dragStart(node, e), false)
 
@@ -187,4 +204,59 @@ function drop(event) {
   event.preventDefault()
   save()
   return false
+}
+
+function editNode(boardId, nodeId) {
+  dialogElement.innerHTML = ""
+  const node = boards[boardId].nodes[nodeId]
+
+  const editorElement = document.createElement("div")
+  editorElement.classList.add("editor")
+  const headerElement = document.createElement("div")
+  headerElement.classList.add("editorHeader")
+  headerElement.innerHTML = `<h2>Note</h2>`
+
+  const inputElement = document.createElement("textarea")
+  inputElement.classList.add("editorBody")
+  inputElement.value = node.title
+  inputElement.style.backgroundColor = node.color
+
+  inputElement.onchange = (e) => {
+    node.title = e.target.value
+  }
+
+  const colorPalette = document.createElement("div")
+  colorPalette.classList.add("colorPalette")
+
+  for (const colorKey in COLORS) {
+    const color = COLORS[colorKey]
+    const colorElement = document.createElement("div")
+    colorElement.style.backgroundColor = color
+    colorElement.classList.add("color")
+    colorElement.onclick = () => {
+      node.color = color
+      inputElement.style.backgroundColor = color
+    }
+    colorPalette.append(colorElement)
+  }
+  headerElement.append(colorPalette)
+  const footerElement = document.createElement("div")
+  footerElement.classList.add("editorFooter")
+
+  const saveButton = document.createElement("button")
+  saveButton.innerText = "Save"
+
+  saveButton.onclick = () => {
+    save()
+    load()
+    dialogElement.close()
+  }
+
+  footerElement.append(saveButton)
+
+  editorElement.append(headerElement, inputElement, footerElement)
+
+  dialogElement.append(editorElement)
+
+  dialogElement.open = true
 }
